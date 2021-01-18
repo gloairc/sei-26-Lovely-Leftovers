3238a0f47ce47784cc2e99e89dd230a7cc680fcd
g00nd0 <g00nd0@MitchT495s.localdomain>
2021-01-19 01:26:26 +0800

    List item details in FoodCard

--- client-react/src/components/foodListing/FoodCard.js
+++ client-react/src/components/foodListing/FoodCard.js
@@ -4,28 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import testImg from "../../images/can-food.jpg";
 // import "./style.css";
 
-const FoodCard = ({ data }) => {
-  const [formData, setFormData] = useState({
-    title: "Food item 1",
-    quantity: 5,
-    category: "Canned stuff",
-    isHalal: true,
-    isVegetarian: false,
-    description: "This is a can of food",
-    bestBefore: "10/03/2666",
-    collectionAddress: "Middle of nowhere Road",
-    contactName: "Some Guy",
-    contactNumber: "666666",
-  });
-
+const FoodCard = ({ foodData }) => {
   return (
     <Card style={{ width: "18rem" }} className="w-50">
+      {foodData.title} ({foodData.quantity})
       <Card.Img variant="top" src={testImg} />
       <Card.Body>
         <Card.Title>
-          {formData.title} ({formData.quantity})
+          {foodData.title} ({foodData.quantity})
         </Card.Title>
-        <Card.Text>{formData.bestBefore}</Card.Text>
+        <Card.Text>{foodData.bestBefore}</Card.Text>
       </Card.Body>
     </Card>
   );
--- client-react/src/components/foodListing/FoodListing.js
+++ client-react/src/components/foodListing/FoodListing.js
@@ -1,37 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FoodCard from "./FoodCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Row, Col, Container } from "react-bootstrap";
+import axios from "axios";
+// use lodash chunking?
 
 const FoodListing = (params) => {
+  const [foodList, setFoodList] = useState();
+  const [batchList, setBatchList] = useState([]);
+  const [fullList, setFullList] = useState([]);
+
+  const getAllBatchId = (batchData) => {
+    return batchData.map((data) => {
+      return data._id;
+    });
+  };
+  const getAllFoodId = (fullList) => {
+    let displayItem = [];
+    for (let i = 0; i < fullList.length; i++) {
+      displayItem.push(
+        fullList[i].foodListings.map((foodItem) => {
+          return foodItem._id;
+        })
+      );
+    }
+  };
+
+  const getDataForDisplay = (responseData) => {
+    responseData.forEach((batch) => {
+      if (batch.status === "active") {
+      }
+    });
+  };
+
+  // const getAllFoodId = (fullList) => {
+  //   let displayItem = [];
+  //   for (let i = 0; i < fullList.length; i++) {
+  //     displayItem.push(
+  //       fullList[i].foodListings.map((foodItem) => {
+  //         return foodItem._id;
+  //       })
+  //     );
+  //   }
+  // };
+
+  // // const renderFoodListing = (batchIds) => {
+  //   let food
+  //   for (let i=0; i<batchIds.length; i++) {
+
+  //   }
+  //   return ( )
+  // }
+
+  useEffect(() => {
+    axios.get(`/batch`).then((response) => {
+      setBatchList(getAllBatchId(response.data));
+      setFullList(response.data);
+      // setBatchList(getAllBatchId(response.data));
+    });
+  }, []);
+
   return (
     <>
       <h2>Food Listing</h2>
       <br />
+      {console.log(fullList)}
       <Container>
-        {/* change to use .map method later */}
-        <Row>
-          <Col>
-            <FoodCard />
-          </Col>
-          <Col>
-            <FoodCard />
-          </Col>
-          <Col>
-            <FoodCard />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <FoodCard />
-          </Col>
-          <Col>
-            <FoodCard />
-          </Col>
-          <Col>
-            <FoodCard />
-          </Col>
-        </Row>
+        {fullList.map((batch) => {
+          console.log(batch.foodListings.length);
+          return batch.foodListings.map((foodItem) => {
+            const foodData = {
+              title: foodItem.title,
+              quantity: foodItem.quantity,
+              bestBefore: foodItem.bestBefore,
+              queryPath: "/" + batch._id + "/" + foodItem._id,
+            };
+            return <FoodCard foodData={foodData} />;
+          });
+
+          // each "data" will have query string, food title, food qty, food bestBefore date
+        })}
       </Container>
     </>
   );
