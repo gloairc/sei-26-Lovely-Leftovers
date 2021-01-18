2a41a5dcf8506e82fa7cafecc8f2348c6b459bf6
g00nd0 <g00nd0@MitchT495s.localdomain>
2021-01-18 11:32:14 +0800

    Add About component, with placeholder image and description

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,11 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Router>
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
