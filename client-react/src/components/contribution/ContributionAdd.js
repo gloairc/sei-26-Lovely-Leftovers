eb3e9838c0a252dc1cb0c6c34a124b89388bf0cd
g00nd0 <g00nd0@MitchT495s.localdomain>
2021-01-19 15:15:18 +0800

    Add/remove food item entry form

--- client-react/src/components/contribution/ContributionAdd.js
+++ client-react/src/components/contribution/ContributionAdd.js
@@ -9,7 +9,55 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const ContributionAdd = () => {
   //   let { batchId, foodId } = useParams();
   const [foodDetails, setFoodDetails] = useState({});
+  const [inputFoodArray, setInputFoodArray] = useState([
+    <Row>
+      <Col>
+        <ItemDetailsAdd />
+        <Button onClick={() => handleRemoveItem()}>-</Button>
+      </Col>
+    </Row>,
+  ]);
   //   const [batchDetails, setBatchDetails] = useState({})
+
+  //test data
+  const [batchDetails, setBatchDetails] = useState({
+    contactPerson: "test data",
+    contactNum: 12345678,
+    collectionAddress: "-10 Anson Avenue",
+    status: "active",
+    foodListings: [
+      {
+        title: "Mango",
+        quantity: 50,
+        category: ["fruit"],
+        isHalal: true,
+        isVegetarian: true,
+        description: "Yellow Juicy Yummy",
+        bestBefore: Date.now(),
+        status: "active",
+      },
+      {
+        title: "Jackfruit",
+        quantity: 44,
+        category: ["fruit"],
+        isHalal: true,
+        isVegetarian: true,
+        description: "Juicy nice cool Crunchy",
+        bestBefore: Date.now(),
+        status: "active",
+      },
+      {
+        title: "Durian",
+        quantity: 99,
+        category: ["fruit"],
+        isHalal: true,
+        isVegetarian: true,
+        description: "Not fresh",
+        bestBefore: Date.now(),
+        status: "inactive",
+      },
+    ],
+  });
   const [batchCreated, setBatchCreated] = useState(false);
   const handleNewBatch = (event) => {
     event.preventDefault();
@@ -18,62 +66,28 @@ const ContributionAdd = () => {
       console.log(response);
     });
   };
-
-  //test data
-  const [batchDetails, setBatchDetails] = useState([
-    {
-      contactPerson: "test data",
-      contactNum: 12345678,
-      collectionAddress: "-10 Anson Avenue",
-      status: "active",
-      foodListings: [
-        {
-          title: "Mango",
-          quantity: 50,
-          category: ["fruit"],
-          isHalal: true,
-          isVegetarian: true,
-          description: "Yellow Juicy Yummy",
-          bestBefore: Date.now(),
-          status: "active",
-        },
-        {
-          title: "Jackfruit",
-          quantity: 44,
-          category: ["fruit"],
-          isHalal: true,
-          isVegetarian: true,
-          description: "Juicy nice cool Crunchy",
-          bestBefore: Date.now(),
-          status: "active",
-        },
-        {
-          title: "Durian",
-          quantity: 99,
-          category: ["fruit"],
-          isHalal: true,
-          isVegetarian: true,
-          description: "Not fresh",
-          bestBefore: Date.now(),
-          status: "active",
-        },
-      ],
-    },
-  ]);
+  const handleInputChange = (e, index) => {};
 
   const handleAddNewItem = () => {
-    console.log("item added");
-    return (
+    setInputFoodArray([
+      ...inputFoodArray,
       <Row>
         <Col>
           <ItemDetailsAdd />
+          <Button onClick={() => handleRemoveItem()}>-</Button>
         </Col>
-      </Row>
-    );
+      </Row>,
+    ]);
+    console.log("item added");
   };
-  const handleRemoveItem = () => {
+  const handleRemoveItem = (index) => {
     console.log("item removed");
+    const list = [...inputFoodArray];
+    list.splice(index, 1);
+    setInputFoodArray(list);
   };
+
+  const handleRemoveClick = (index) => {};
 
   //   useEffect(() => {
   //     axios.get(`/batch/${batchId}`).then((response) => {
@@ -88,15 +102,10 @@ const ContributionAdd = () => {
   //   }, []);
 
   return (
-    <Form onClick={handleNewBatch}>
-      <Row>
-        <Col>
-          <ItemDetailsAdd />
-        </Col>
-      </Row>
-
+    <Form onSubmit={handleNewBatch}>
+      {inputFoodArray}
       <Button onClick={() => handleAddNewItem()}>+</Button>
-      <Button onClick={() => handleRemoveItem()}>-</Button>
+
       <Row>
         <Col>
           <Button type="submit" style={{ margin: "10px 0" }}>
