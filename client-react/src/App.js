f41495f277cde2f5cf0c0d2349a06b39da3af09d
Bairong Huang <brongh90@gmail.com>
2021-01-20 12:10:29 +0800

    m

---
+++ client-react/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import OneItem from "./components/foodListing/OneItem";
+import FoodListing from "./components/foodListing/FoodListing";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Router>
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route path="/listings">
+            <FoodListing />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
