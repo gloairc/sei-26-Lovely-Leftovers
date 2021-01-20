2a090f5eac635c32754272d0d112f67876986d30
Bairong Huang <brongh90@gmail.com>
2021-01-20 14:32:47 +0800

    m

--- server.js
+++ server.js
@@ -50,7 +50,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("./client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client-react", "build", "index.html")
+    );
   });
 }
 
