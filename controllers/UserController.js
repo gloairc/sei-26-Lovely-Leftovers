de880fac93d606b9c71df987ba70ae8f764488f4
Gloria <gloria_cheng7@yahoo.com>
2021-01-21 23:30:26 +0800

    checks if username exist in database onBlur during account creation

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -17,14 +17,26 @@ const Batch = require("../models/batch");
 
 // INDEX (show all users - admin access only)
 router.get("/", (req, res) => {
-    User.find({}, (error, users) => {
-        if (error) {
-            res.send(error);
-        } else {
-            res.send(users);
-        }
-    });
+    if (req.query.username) { //if there is a query, check if it exist
+        console.log("req.query.username", req.query.username);
+        User.find({ username: req.query.username }, (error, oneUser) => {
+            if (error) {
+                res.status(StatusCodes.BAD_REQUEST).send(error);
+            } else { //user exist
+                res.status(StatusCodes.OK).send(oneUser);
+            }
+        })
+    } else { //return all users
+        User.find({}, (error, users) => {
+            if (error) {
+                res.send(error);
+            } else {
+                res.send(users);
+            }
+        });
+    }
 });
+
 
 // SEEDING
 router.get("/seed", (req, res) => {
